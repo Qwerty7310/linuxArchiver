f1442b7bf9fdb36f230a7bf70b0f0df223891ff3
Qwerty7310 <daniyl.2004@yandex.ru>
2024-09-18 01:52:37 +0300

    работа с параметрами командной строки

--- Makefile
+++ Makefile
@@ -1,23 +1,29 @@
 CC = gcc
-CFLAGS = 
-TARGET = linux_archiver
+TARGET1 = linux_archiver
+TARGET2 = linux_unarchiver
 
-all: clean $(TARGET)
+all: clean $(TARGET1) $(TARGET2)
 
-$(TARGET): linux_archiver.o my_dirent.o archiver.o unarchiver.o
+$(TARGET1): linux_archiver.o my_dirent.o archiver.o
+	$(CC) $^ -o $@
+
+$(TARGET2): linux_unarchiver.o my_dirent.o unarchiver.o
 	$(CC) $^ -o $@
 
 linux_archiver.o: linuxArchiver.c
-	$(CC) $(CFLAGS) -c $^ -o $@
+	$(CC) -DARCH -c $^ -o $@
+
+linux_unarchiver.o: linuxArchiver.c
+	$(CC) -DUNARCH -c $^ -o $@
 
 my_dirent.o: my_dirent.c
-	$(CC) $(CFLAGS) -c $^ -o $@
+	$(CC) -c $^ -o $@
 
 archiver.o: archiver.c
-	$(CC) $(CFLAGS) -c $^ -o $@
+	$(CC) -c $^ -o $@
 
 unarchiver.o: unarchiver.c
-	$(CC) $(CFLAGS) -c $^ -o $@
+	$(CC) -c $^ -o $@
 
 clean:
 	rm -rf *.o 
@@ -28,4 +34,36 @@ rebuild: clean all
 
 clang:
 	clang-format -i *.c *.h
+
+# CC = gcc
+# CFLAGS = 
+# TARGET = linux_archiver
+
+# all: clean $(TARGET)
+
+# $(TARGET): linux_archiver.o my_dirent.o archiver.o
+# 	$(CC) $^ -o $@
+
+# linux_archiver.o: linuxArchiver.c
+# 	$(CC) $(CFLAGS) -c $^ -o $@
+
+# my_dirent.o: my_dirent.c
+# 	$(CC) $(CFLAGS) -c $^ -o $@
+
+# archiver.o: archiver.c
+# 	$(CC) $(CFLAGS) -c $^ -o $@
+
+# unarchiver.o: unarchiver.c
+# 	$(CC) $(CFLAGS) -c $^ -o $@
+
+# clean:
+# 	rm -rf *.o 
+# 	rm -rf ../../archive.arch
+# 	rm -rf ../test*
+
+# rebuild: clean all
+
+# clang:
+# 	clang-format -i *.c *.h
+
 
--- linuxArchiver.c
+++ linuxArchiver.c
@@ -1,99 +1,39 @@
-#include <fcntl.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-// #include <libgen.h> // for basename()
-
-// #include <unistd.h>
-
-#include <string.h>
-#include <sys/stat.h>
-// #include <sys/syscall.h>
-
+#ifdef ARCH
 #include "archiver.h"
-#include "my_dirent.h"
+#endif
+
+#ifdef UNARCH
 #include "unarchiver.h"
+#endif
+
+#include "my_dirent.h"
 
 #define OK 0
 #define ERROR 1
 #define BUF_SIZE 1024
 
-// int readFileInfo(FILE *f_info);
+int main(int argc, char *argv[]) {
+    if (argc == 3) {
+#ifdef ARCH
+        char *path1 = "/home/qwerty/OS/test3/";
+        char *path2 = "../../test1/";
+        char *path3 = "../../dferfe/";
 
-int main() {
-    char *path1 = "/home/qwerty/OS/test3/";
-    char *path2 = "../../test1/";
-    char *path3 = "../../dferfe/";
+        // FILE *res = archive(path1, "../../archive.arch");
+        puts(argv[1]);
+        FILE *res =
+            archive(argv[1], argv[2]);  // ./linux_archiver '/home/qwerty/OS/test3/' '../../archive.arch'
+        fclose(res);
+#endif
 
-    FILE *res = archive(path1, "../../archive.arch");
+#ifdef UNARCH
+        unarchive(argv[1], argv[2]);  // ./linux_unarchiver '../../archive.arch' '/home/qwerty/OS/Lab_1/'
+#endif
 
-    fclose(res);
-
-    unarchive("../../archive.arch", "/home/qwerty/OS/Lab_1/");
-    // readFileInfo(res);
-
-    /*
-    puts(realpath(path1, buf));
-    if (realpath(path1, buf) != NULL) {
-        char *dir_name = basename(buf);  // Получаем имя каталога
-        printf("Имя каталога: %s\n", dir_name);
     } else {
-        perror("Ошибка получения реального пути");
+        perror("incorrect input");
     }
-    */
-
-    // f_info = fopen("f_info", "r");
-    // for (int i = 0; i < cnt; i++) {
-    //     readFileInfo(f_content);
-    // }
-    // fclose(f_info);
-
-    // f_content = fopen("f_content", "r");
-    // for (int i = 0; i < 20; i++) {
-    //     char c = getc(f_content);
-    //     printf("%c", c);
-    // }
-    // fclose(f_content);
-
-    // DIR *directory = openDir(path1);
-    // for (int i = 0; i < 10; i++) {
-    //     readDir(directory);
-    // }
-
-    // printDir(path2);
 
     return 0;
 }
-
-// int readFileInfo(FILE *file) {
-//     int cnt_dir;
-
-//     char root_dir_name[BUF_SIZE];
-//     fread(root_dir_name, sizeof(root_dir_name), 1, file);
-
-//     fscanf(file, "%d", &cnt_dir);
-
-//     printf("root_dir: %s, %d\n", root_dir_name, cnt_dir);
-
-//     for (int i = 0; i < cnt_dir; i++) {
-//         char buffer_dir[BUF_SIZE];
-//         fread(buffer_dir, BUF_SIZE, 1, file);
-//         printf("dir: %s\n", buffer_dir);
-//     }
-
-//     int cnt_file;
-//     fscanf(file, "%d", &cnt_file);
-
-//     struct FileInfo *buffer_files = (struct FileInfo *)malloc(cnt_file * sizeof(struct FileInfo));
-//     fread(buffer_files, sizeof(struct FileInfo), cnt_file, file);
-
-//     // fread(buffer, sizeof(struct file_info), 1, f_info);
-
-//     for (int i = 0; i < cnt_file; i++) {
-//         printf("path: %s\n", buffer_files[i].d_path);
-//         printf("name: %s\n", buffer_files[i].d_name);
-//     }
-
-//     return 0;
-// }
